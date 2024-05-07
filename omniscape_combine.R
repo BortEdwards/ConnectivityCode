374930f0e9a9cdd9dfd72e41ede475eb36dc7599
Robert David Edwards <42812596+BortEdwards@users.noreply.github.com>
2024-05-07 14:02:19 -0600

    Update omniscape_combine.R
    Added calculation of raster quantiles. Refined notes

--- omniscape_combine.R
+++ omniscape_combine.R
@@ -1,17 +1,26 @@
+# Post-run Omniscape processing:
+# 1. - rescale, and stack outputs from different Omniscape window size/scale analyses
+# 2. - bin stacked layers into quantiles by geographic area/pixel number
+# 3. - trial code to normalize, reclassify and stack different window size/scale analyses (can be appropriate if not using the normalized Omniscape output)
 
+########################
+# Packages
 library(dplyr)
 library(terra)
+library(raster)
+########################
 
+######################## 1. stack outputs from differnt Omniscape window size/scale analyses (mostly approproate for already normalized Ominscape outputs)
 
 outpth <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/Processed Output"
 wopt <- list(gdal=c("COMPRESS=LZW", "TFW=YES", "BIGTIFF=YES"))
 
 # Mean ntiles of Raw Current ------------------------------------------------
-r2.5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO2.5miles37blocks/Output/AllCO2.5miles37blocks.tif"
-r5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO5miles75blocks/Output/AllCO5miles75blocks.tif"
-r10 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO10miles151blocks/Output/AllCO10miles151blocks.tif"
-r25 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO25miles375blocks/Output/AllCO25miles375blocks.tif"
-r50 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO50miles751blocks/Output/AllCO50miles751blocks.tif"
+r2.5 <- "C:/Users/C836876692/OneDrive - Colostate/Omniscape/Omniscape CO-wide/Omniscape Output Rasters/AllCO2.5miles37blocks_normalized.tif"
+r5 <- "C:/Users/C836876692/OneDrive - Colostate/Omniscape/Omniscape CO-wide/Omniscape Output Rasters/AllCO5miles75blocks_normalized.tif"
+r10 <- "C:/Users/C836876692/OneDrive - Colostate/Omniscape/Omniscape CO-wide/Omniscape Output Rasters/AllCO10miles151blocks_normalized.tif"
+r25 <- "C:/Users/C836876692/OneDrive - Colostate/Omniscape/Omniscape CO-wide/Omniscape Output Rasters/AllCO25miles375blocks_normalized.tif"
+r50 <- "C:/Users/C836876692/OneDrive - Colostate/Omniscape/Omniscape CO-wide/Omniscape Output Rasters/AllCO50miles751blocks_normalized.tif"
 
 #make raster objects
 ras2.5 <- rast(r2.5)
@@ -50,16 +59,41 @@ names(plist) <- c("radius2.5", "radius5", "radius10", "radius25", "radius50")
 ptstack <- rast(plist)
 
 # Calculate the mean of the stacked percentile raster and write to a new file
-testout <- app(ptstack, fun=mean, filename=file.path(outpth, "CombinedRawAllCOOmnitestOutput.tif"), wopt=wopt)
+combRast <- app(ptstack, fun=mean, filename=file.path(outpth, "CompiledNormalizedOutputs.tif"), wopt=wopt)
+
+######################## 2. - bin composite layer into quantiles with equal pixel number
+
+# Extract values from the raster
+raster_values <- values(combRast)
+
+# Calculate terciles
+quantiles <- quantile(raster_values, probs = c(0, 1/3, 2/3, 1), na.rm = TRUE)
+
+### alternate quantiles
+## 5 bins (quintiles)
+## quantiles <- quantile(raster_values, probs = seq(0, 1, by = 0.25), na.rm = TRUE)
+
+## 10 bins (deciles)
+## quantiles <- quantile(raster_values, probs = seq(0, 1, by = 0.1), na.rm = TRUE)
+
+# Reclassify raster into quantiles
+reclassified_raster <- classify(combRast, quantiles, include.lowest = TRUE)
+
+# Plot the reclassified raster
+plot(reclassified_raster)
+
+# Save the reclassified raster as a .tif file
+output_file <- "CompiledNormalizedOutputsTerciles.tif"
+writeRaster(reclassified_raster, filename = output_file, overwrite = TRUE)
 
 
-########################
-# Mean of Rescaled Normalized Current ---------------------------------------
-n2.5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO2.5miles37blocks/Output/AllCO2.5miles37blocks_normalized.tif"
-n5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO5miles75blocks/Output/AllCO5miles75blocks_normalized.tif"
-n10 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO10miles151blocks/Output/AllCO10miles151blocks_normalized.tif"
-n25 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO25miles375blocks/Output/AllCO25miles375blocks_normalized.tif"
-n50 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO50miles751blocks/Output/AllCO50miles751blocks_normalized.tif"
+######################## 3. - trial code to normalize and rescale Omniscape output layers based on pixel values (can be appropriate if not using the already normalized Omniscape output)
+# load files ---------------------------------------
+n2.5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO2.5miles37blocks/Output/AllCO2.5miles37blocks.tif"
+n5 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO5miles75blocks/Output/AllCO5miles75blocks.tif"
+n10 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO10miles151blocks/Output/AllCO10miles151blocks.tif"
+n25 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO25miles375blocks/Output/AllCO25miles375blocks.tif"
+n50 <- "C:/Users/C836876692/Desktop/Omniscape Test/Omniscape CO-wide/AllCO50miles751blocks/Output/AllCO50miles751blocks.tif"
 
 #make raster objects
 nras2.5 <- rast(n2.5)
@@ -94,5 +128,5 @@ names(outlist) <- c("radius2.5", "radius5", "radius10", "radius25", "radius50")
 
 #Stack the rescaled and normalized current rasters
 normout <- app(outlist, fun=mean, 
-               filename=file.path(outpth, "CompiledNormalizedAllCOOmintestOutput.tif"), 
+               filename=file.path(outpth, "CompiledNormalizedRescaledfromRawOutput.tif"), 
                wopt=wopt)
